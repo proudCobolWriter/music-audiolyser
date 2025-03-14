d621c242af878e7c9e2ff36027c6c07d4e9ff60e
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2025-03-14 20:11:29 +0100

    viteconfig laid down

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
+  base: "/static/",
+  build: {
+    manifest: "manifest.json",
+    outDir: "./assets",
+    rollupOptions: {
+      //input: "/path/to/main.js"
+    }
+  },
   plugins: [react()],
-})
+}) satisfies UserConfig
