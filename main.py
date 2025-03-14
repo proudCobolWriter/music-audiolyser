d1a6804c48c17c5d73f8396f3116af8e9a63e437
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2025-03-14 19:14:37 +0100

    Fix versioning check

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import django
 REQUIRED_PY_VERSION = "3.9.18"
 
 # Starting the node/webserver
-if sys.version_info[0:3] != tuple(REQUIRED_PY_VERSION.replace(".", "")):
-    raise UserWarning(f"The interpreter is running the wrong version of python (currently: {".".join(map(str, sys.version_info[0:3]))}) which could cause issues with Essentia.\nConsider using the {REQUIRED_PY_VERSION} one")
+if sys.version_info[0:3] != tuple(map(int, REQUIRED_PY_VERSION.split("."))):
+    raise UserWarning(f"The interpreter is running the wrong version of python (currently: {'.'.join(map(str, sys.version_info[0:3]))}) which could cause issues with Essentia.\nConsider using the {REQUIRED_PY_VERSION} one")
 
 print("Current django version is %s" % django.get_version())
 
