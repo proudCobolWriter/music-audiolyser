164049f626d06675ba3ee71f3fac2fc31791946f
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2025-03-16 13:00:30 +0100

    Final (I hope) App.tsx test

--- src/vite/src/App.tsx
+++ src/vite/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
                 <a href="https://react.dev" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
-                                            </div>
+            </div>
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
