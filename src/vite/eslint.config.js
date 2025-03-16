e96daca0f00ce1d4a89ac72fa2534368562d6bf0
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2025-03-16 12:31:53 +0100

    Cease eslint yap

--- src/vite/eslint.config.js
+++ src/vite/eslint.config.js
@@ -20,7 +20,7 @@ export default tseslint.config(
         rules: {
             ...reactHooks.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-            "@typescript-eslint/ban-ts-comment": false,
+            "@typescript-eslint/ban-ts-comment": ["error", false],
         },
     },
 );
