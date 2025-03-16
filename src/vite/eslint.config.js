621e2e1d84f564c7d7b92d72409092bb638b802c
proudCobolWriter <77924245+proudCobolWriter@users.noreply.github.com>
2025-03-16 13:00:07 +0100

    Major eslint adjustments

--- src/vite/eslint.config.js
+++ src/vite/eslint.config.js
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import globals from "globals";
 import importPlugin from "eslint-plugin-import";
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -8,23 +10,24 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
     { ignores: ["./dist"] },
     {
-        extends: [
-            js.configs.recommended,
-            ...tseslint.configs.recommended,
-            "plugin:import/recommended",
-            "plugin:import/typescript",
-        ],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ["**/*.{ts,tsx}"],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
+        languageOptions: {
+            parser: typescriptParser,
+        },
         plugins: {
             "react-hooks": reactHooks,
             "react-refresh": reactRefresh,
+            import: importPlugin,
+            "@typescript-eslint": typescriptPlugin,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
+            ...typescriptPlugin.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
             "@typescript-eslint/ban-ts-comment": "off",
             "import/first": "off",
