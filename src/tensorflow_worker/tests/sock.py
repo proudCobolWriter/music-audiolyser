6d03ef52d1133cce6e21872b095b261a44a7c587
Proud Cobol Writer <77924245+proudCobolWriter@users.noreply.github.com>
2026-05-30 22:48:45 +0200

    Refactor socket

--- src/tensorflow_worker/tests/sock.py
+++ src/tensorflow_worker/tests/sock.py
@@ -17,7 +17,19 @@ p1.start()
 time.sleep(1)
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET -> IPv4, SOCK_STREAM -> ICP
 ls.connect(("127.0.0.1", 65432))
-ls.sendall("STOP".encode())
+# ls.sendall("STOP".encode())
+
+
+def utf8len(s):
+    return len(s.encode("utf-8"))
+
+
+bufferLimitStr = "a" * 1024
+
+assert utf8len(bufferLimitStr) == 1024
+
+ls.sendall((bufferLimitStr + "b").encode())  # addition of "b" will send a second packet
+
 time.sleep(5)
 ls.shutdown(socket.SHUT_RDWR)
 ls.close()
--- src/tensorflow_worker/tfsocket.py
+++ src/tensorflow_worker/tfsocket.py
@@ -1,6 +1,5 @@
 import socket
 import logging
-import asyncio
 
 from tensorflow_worker.logging.custom import CustomFormatter as cf
 
@@ -19,11 +18,12 @@ logger.addHandler(colorlog)
 # for level in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
 #     logger.__getattribute__(level.lower())(f"Printing test string with {level = }")
 
-# Constants
+# CONSTANTS
 
 HOST = "127.0.0.1"  # should be localhost if the tensorflow worker is on the same server as the websites backend
 PORT = 65432  # in which case, the socket shouldn't be available to other IPv4 interfaces than localhost
 RECEIVE_BUFFER = 1024  # bits
+CLIENT_POOL_MAX_SIZE = 10  # rough estimate of the number of services to be expected
 
 # Socket manager class export
 
@@ -37,11 +37,11 @@ class TFWorkerSocket:
         if not (self.PORT >= 1024 and self.PORT <= 2**16):
             logger.warning("Port should be within the user registered port range")
 
-        self.Running = False
+        self.running = False
         self.Socket = None
 
     def __repr__(self) -> None:
-        fields = ", ".join(f"{i!r}={v!r}" for (i, v) in zip(self.__dict__.keys(), self.__dict__.values()))
+        fields = ", ".join(f"{k!r}={v!r}" for k, v in self.__dict__.items())
         return f"{self.__class__.__name__}({fields})"
 
     def __str__(self) -> None:
@@ -53,23 +53,23 @@ class TFWorkerSocket:
             return logger.warning("A socket has already been created. Consider closing it first.")
 
         try:
-            self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET -> IPv4, SOCK_STREAM -> ICP
+            self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET -> IPv4, SOCK_STREAM -> TCP
             self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.Socket.bind((self.HOST, self.PORT))
-            self.Socket.listen(1)  # allow only one connection from clients
+            self.Socket.listen(CLIENT_POOL_MAX_SIZE)
             self.Socket.setblocking(True)  # make socket operations yield
             logger.info(f"The socket is now listening on {self.HOST}:{self.PORT}")
         except socket.error:
             logger.error("Failed to setup the socket:", exc_info=True)
             self.Socket = None
         else:
-            self.Running = True
+            self.running = True
             self.handleSocket()
         finally:
             self.closeSocket()
 
     def handleSocket(self) -> None:
-        while self.Running:
+        while self.running:
             try:
                 if self.__dict__.get("Connection") is None:
                     self.Connection, self.ClientAddress = self.Socket.accept()  # yields for a connection
@@ -77,18 +77,17 @@ class TFWorkerSocket:
                         f"Established connection with {self.ClientAddress} on {self.HOST}:{self.PORT} as expected"
                     )
 
-                data = self.Connection.recv(1024)
+                data = self.Connection.recv(RECEIVE_BUFFER)
                 logger.debug(f"Received packet: {data = }")
 
                 if not data:  # or equals to ''
                     logger.info("Socket connection has been severed, awaiting new connection")
-                    # break
                     self.Connection, self.ClientAddress = None, None
                     continue
 
                 decodedBuffer = data.decode()
 
-                if decodedBuffer == "STOP":
+                if decodedBuffer.strip() == "STOP":
                     logger.info('Socket connection is closing gracefully after a "STOP" request')
                     break
 
@@ -103,15 +102,16 @@ class TFWorkerSocket:
     def closeSocket(self) -> None:
         if self.Socket is None:
             return
+        else:
+            self.Socket.shutdown(socket.SHUT_RDWR)
+            self.Socket.close()
+            self.Socket = None
 
-        self.Connection.shutdown(socket.SHUT_RDWR)
-        self.Socket.shutdown(socket.SHUT_RDWR)
-        self.Connection.close()
-        self.Socket.close()
+        if self.Connection:
+            self.Connection.shutdown(socket.SHUT_RDWR)
+            self.Connection.close()
+            self.Connection, self.ClientAddress = None, None
 
-        self.Socket = None
-        self.Connection, self.ClientAddress = None, None
-
-        self.Running = False
+        self.running = False
 
         logger.info("The socket is now closed")
